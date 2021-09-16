27278bbb020178ad90d78d9df94f027ff6334633
Tesla-Atoz <teslaatozinspires7@gmail.com>
2021-09-16 19:03:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Button,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import React from "react";
+import useStyles from "./app-styles.js";
 
-function App() {
+export default function App() {
+  const classes = useStyles();
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottoms
+            >
+              Dinesh Pandikona
+            </Typography>
+
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello! I am photo album. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Atque iste optio quae.
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify={"center"}>
+                <Grid item>
+                  <Button variant={"contained"} color={"primary"}>
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant={"outlined"} color={"primary"}>
+                    secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map(() => (
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      THi is a media card. You can use this section to tell the
+                      conntent
+                    </Typography>
+                  </CardContent>
+
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
     </div>
   );
 }
-
-export default App;
